59746d36a4cca4b7f852f57c8d861ddf50a27463
thiotimoo <timoothio@gmail.com>
2023-12-28 12:39:32 +0700

    Personal Branding

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
     return (
-        <div className='text-xl text-body text-center w-full p-6  flex flex-col max-w-screen-lg mx-auto'>
-            <hr className="h-px my-6 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="mx-auto flex w-full max-w-screen-lg flex-col  p-6 text-center text-xl text-body">
+            <hr className="my-6 h-px border-0 bg-gray-200 dark:bg-gray-700"></hr>
             <h3>
-                ✨ Created by <span className='font-bold bg-secondary text-black px-2'>@thio.timoo</span>
+                ✨ Created by{" "}
+                <Link
+                    className="bg-secondary px-2 font-bold text-black"
+                    href="https://github.com/thiotimoo"
+                >
+                    @thiotimoo
+                </Link>
             </h3>
             <h3>
                 Powered by <b>Jikan.moe API</b>
             </h3>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
